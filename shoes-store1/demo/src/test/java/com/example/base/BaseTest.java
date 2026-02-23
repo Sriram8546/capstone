16b5f961f1e2de84b1d3ac9b0bdac4765a522058
M.Sriram <sriramchowdaary@gmail.com>
2026-02-23 16:58:14 +0530

    '2nd'

--- shoes-store1/demo/src/test/java/com/example/base/BaseTest.java
+++ shoes-store1/demo/src/test/java/com/example/base/BaseTest.java
@@ -1,5 +1,4 @@
 package com.example.base;
-
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
@@ -8,25 +7,40 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-
-public abstract class BaseTest {
+public class BaseTest {
 
     protected WebDriver driver;
+
     @BeforeMethod(alwaysRun = true)
-public void setUp() {
+    public void setUp() {
 
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--disable-notifications");
+        ChromeOptions options = new ChromeOptions();
 
-    driver = new ChromeDriver(options);
+        // 🔥 HEADLESS MODE FOR GITHUB
+        String headless = System.getProperty("headless");
 
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        if ("true".equalsIgnoreCase(headless)) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+        }
 
-    
-    driver.get("http://127.0.0.1:5500/shoes-store1/shoes-store/index.html");
-}
+        options.addArguments("--disable-notifications");
 
+        driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // open app
+        driver.get("http://localhost:3000/");
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
@@ -34,8 +48,45 @@ public void setUp() {
             driver.quit();
         }
     }
-
-    protected WebDriver getDriver() {
-        return driver;
-    }
 }
+// package com.example.base;
+
+// import java.time.Duration;
+
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeOptions;
+// import org.testng.annotations.AfterMethod;
+// import org.testng.annotations.BeforeMethod;
+
+
+// public abstract class BaseTest {
+
+//     protected WebDriver driver;
+//     @BeforeMethod(alwaysRun = true)
+// public void setUp() {
+
+//     ChromeOptions options = new ChromeOptions();
+//     options.addArguments("--disable-notifications");
+
+//     driver = new ChromeDriver(options);
+
+//     driver.manage().window().maximize();
+//     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    
+//     driver.get("http://127.0.0.1:5500/shoes-store1/shoes-store/index.html");
+// }
+
+
+//     @AfterMethod(alwaysRun = true)
+//     public void tearDown() {
+//         if (driver != null) {
+//             driver.quit();
+//         }
+//     }
+
+//     protected WebDriver getDriver() {
+//         return driver;
+//     }
+// }
